413296df56b0bb650bfe5bb30c2b067840adc90b
HanPengfei <1060003755@qq.com>
2022-06-30 16:58:27 +0800

    定时显示

---
+++ Timing/showPic.py
@@ -0,0 +1,23 @@
+import time
+import matplotlib.pyplot as plt
+
+minute = 60
+
+
+def show_pic():
+    now = time.localtime()
+    while True:
+        if(now.tm_min in range(0,10)):
+            img_path = r"preview.jpg"
+            img = plt.imread(img_path)
+            plt.figure("站一站")
+            plt.imshow(img)
+            plt.show(block=False)
+            plt.pause(5 * minute)
+            plt.close()
+        else:
+            time.sleep(5 * minute)
+
+
+if __name__ == '__main__':
+    show_pic()
